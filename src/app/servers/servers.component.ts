f7063f963ff78267a2727ed5ccb06b3bb80b3583
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-01-06 19:29:34 +0530

    Setting up Child (Nested) Routes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,11 +10,7 @@ import { ServersService } from "./servers.service";
 export class ServersComponent implements OnInit {
   public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(
-    private serversService: ServersService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private serversService: ServersService, private router: Router) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
